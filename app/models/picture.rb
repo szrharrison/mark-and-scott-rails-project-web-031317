f4b5648183ad09b7fdb869dd6d3258dcc6e117f6
markdegroat <mhdegroat@gmail.com>
2017-04-19 15:47:26 -0400

    Fixed the method in picture model class to save the image with a unique file name.

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -23,9 +23,49 @@ class Picture < ApplicationRecord
     self.categories = categories_array
   end
 
+  #modifier methods
   def greyscale
+    #grabs path of image on our server
     path = self.image.path
+    #create a new "Image List" object from the image at the path
     image = ImageList.new(path)
-    image.display
+    #uses the quantize method to set the number of colors used in the image to 2
+    image = image.quantize(nc=2)
+    #this saves the file in the location it was before
+    image.write(path.gsub(/\/original\/(.+\.)(...|....)/, '/original/grey-scale-\1\2'))
+    #use this for testing, need to have X11 runnning for it to work
+    #image.display
+  end
+
+  def edge
+    #grabs path of image on our server
+    path = self.image.path
+    #create a new "Image List" object from the image at the path
+    image = ImageList.new(path)
+    #uses the quantize method to set the number of colors used in the image to 2
+    image = image.edge
+    #this saves the file in the location it was before
+    image.write(path.gsub(/\/original\/(.+\.)(...|....)/, '/original/edge-detected-\1\2'))
+    #use this for testing, need to have X11 runnning for it to work
+    #image.display
+  end
+  #image evaluators
+  def monochrome?
+    path = self.image.path
+    #create a new "Image List" object from the image at the path
+    image = ImageList.new(path)
+    #uses the quantize method to set the number of colors used in the image to 2
+    image.monochrome?
+    #this will return true if the image is monochrome, then we can add that as a category
+  end
+
+  def gray?
+    path = self.image.path
+    #create a new "Image List" object from the image at the path
+    image = ImageList.new(path)
+    #uses the quantize method to set the number of colors used in the image to 2
+    image.gray?
+    #this will return true if the image is monochrome, then we can add that as a category
+
   end
 end
