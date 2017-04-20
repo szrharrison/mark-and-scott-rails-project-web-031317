c857c23249037c48c325d78740981d19da23aaf6
szrharrison <s.z.r.harrison@gmail.com>
2017-04-20 13:53:42 -0400

    Fixed adding categories so that a user can't create categories

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,5 +1,6 @@
 class PicturesController < ApplicationController
   before_action :set_picture, only: [:show, :edit, :update]
+  before_action :set_categories, only: [:new, :edit]
   before_action :restrict_to_users, except: [:index, :show]
 
   def new
@@ -8,12 +9,11 @@ class PicturesController < ApplicationController
 
   def create
     @picture = Picture.new( picture_params )
-    @picture.category_list = params.require( :categories )
     @picture.user = current_user
     if @picture.save
       redirect_to picture_path( @picture )
     else
-      render :new
+      redirect_to :new, notice: @picture.errors.full_messages
     end
   end
 
@@ -21,18 +21,11 @@ class PicturesController < ApplicationController
     @pictures = Picture.all
   end
 
-  def edit
-    @picture_categories = @picture.categories.map do |category|
-      category.name
-    end
-    @picture_categories = @picture_categories.join(", ")
-  end
-
   def update
     if @picture.update( picture_params )
       redirect_to picture_path( @picture )
     else
-      render :edit
+      redirect_to :edit, notice: @picture.errors.full_messages
     end
   end
 
@@ -42,7 +35,13 @@ class PicturesController < ApplicationController
     @picture = Picture.find( params[:id] )
   end
 
+  def set_categories
+    @categories = Category.all
+  end
+
   def picture_params
-    params.require( :picture ).permit( :image, :name )
+    params_hash = params.require( :picture ).permit( :image, :name )
+    params_hash[:category_ids] = params.require( :picture ).require( :category_ids )
+    params_hash
   end
 end
